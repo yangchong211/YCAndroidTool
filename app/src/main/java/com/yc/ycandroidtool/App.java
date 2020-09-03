956e1aac19c223683fa55156204dbf4e0b6c9b7f
杨充 <yangchong@doushen.com>
2020-09-03 19:09:34 +0800

    更新文档

--- app/src/main/java/com/yc/ycandroidtool/App.java
+++ app/src/main/java/com/yc/ycandroidtool/App.java
@@ -28,6 +28,7 @@ public class App extends Application {
              */
             @Override
             public void againStartApp() {
+                System.out.println("崩溃重启----------againStartApp------");
                 CrashToolUtils.reStartApp1(App.this,2000);
                 //CrashToolUtils.reStartApp2(App.this,2000, MainActivity.class);
                 //CrashToolUtils.reStartApp3(App.this);
@@ -39,6 +40,7 @@ public class App extends Application {
              */
             @Override
             public void recordException(Throwable ex) {
+                System.out.println("崩溃重启----------recordException------");
                 //自定义上传crash，支持开发者上传自己捕获的crash数据
                 //StatService.recordException(getApplication(), ex);
             }
