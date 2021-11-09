9a141b1fa0301f12d70e9dbf553bb5655ca28f9b
yangchong <yangchong211@163.com>
2021-11-09 10:40:46 +0800

    提交代码

--- CatonHelperLib/src/main/java/com/yc/catonhelperlib/fps/PerformanceManager.java
+++ CatonHelperLib/src/main/java/com/yc/catonhelperlib/fps/PerformanceManager.java
@@ -36,6 +36,12 @@ public class PerformanceManager {
     }
 
 
+    /**
+     * 定时任务
+     * 1.使用线程，执行一个异步定时任务，每1000ms执行一次，用于统计1秒内的帧率。
+     * 2.使用 Choreographer.getInstance().postFrameCallback(this) 注册 VSYNC 信号回调监听，当 VSYNC 信号返回时，会执行 doFrame 回调函数。
+     * 3.在 doFrame 方法中，我们统计每秒内的执行次数，以及记录当前帧的时间，并注册一下次监听。
+     */
     private class FrameRateRunnable implements Runnable, Choreographer.FrameCallback {
         
         private int totalFramesPerSecond;
@@ -57,6 +63,7 @@ public class PerformanceManager {
 
         public void doFrame(long frameTimeNanos) {
             ++totalFramesPerSecond;
+            //注册下一帧回调
             Choreographer.getInstance().postFrameCallback(this);
             writeFpsDataIntoFile();
         }
@@ -72,12 +79,20 @@ public class PerformanceManager {
         mRateRunnable = new FrameRateRunnable();
     }
 
+    /**
+     * 开始
+     */
     public void startMonitorFrameInfo() {
         mMainHandler.postDelayed(mRateRunnable, 1000L);
+        //注册下一帧回调
         Choreographer.getInstance().postFrameCallback(mRateRunnable);
     }
 
+    /**
+     * 暂停
+     */
     public void stopMonitorFrameInfo() {
+        //移除下一帧回调
         Choreographer.getInstance().removeFrameCallback(mRateRunnable);
         mMainHandler.removeCallbacks(mRateRunnable);
     }
@@ -93,6 +108,7 @@ public class PerformanceManager {
     }
 
     public void destroy() {
+        //销毁
         stopMonitorFrameInfo();
     }
 
