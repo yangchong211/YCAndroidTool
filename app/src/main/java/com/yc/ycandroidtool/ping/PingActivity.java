3daac4f6abb61dd98f38acdaf3f9dd935b3d54e0
yangchong <yangchong211@163.com>
2022-02-07 12:55:11 +0800

    完善笔记

--- app/src/main/java/com/yc/ycandroidtool/ping/PingActivity.java
+++ app/src/main/java/com/yc/ycandroidtool/ping/PingActivity.java
@@ -3,6 +3,7 @@ package com.yc.ycandroidtool.ping;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -84,5 +85,12 @@ public class PingActivity extends AppCompatActivity implements View.OnClickListe
         mPing.setUserId("342");
         mPing.setVersionName("1.1.1");
         mPing.pingHost("https://www.wanandroid.com/article/list/0/json");
+
+        mBtn1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
     }
 }
